e9a5f3047729424735c2ba03cef253058d0db690
Ddudooo <igoa.work@gmail.com>
2021-08-26 18:09:07 +0900

    자바스크립트 기본 자료형
    여덟가지 기본 자료형

---
+++ javascript/type/basic/typeSample.js
@@ -0,0 +1,13 @@
+const numberType = 12;
+const bigIntType = 123456789123456789123456789n;
+const stringType = 'ab';
+const booleanType = true;
+
+console.log(typeof numberType, typeof bigIntType, typeof stringType, typeof booleanType);
+
+const objectType = {key:'value'};
+const symbolType = Symbol('abc');
+const undefinedType = undefined;
+const nullType = null;
+
+console.log(typeof objectType, typeof symbolType, typeof undefinedType, typeof nullType);
